97933e0805a4f870a68e4b9ce5f1b4c84b9600a6
viniciussec <viniciussec@hotmail.com>
2020-03-22 17:45:27 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/example/viniciussec/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.example.viniciussec.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.viniciussec.domain.User;
+import com.example.viniciussec.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria= new User(null, "Maria Brown", "maria@gmail.com");
+		User alex= new User(null, "Alex Green", "alex@gmail.com");
+		User bob= new User(null, "BobGrey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
