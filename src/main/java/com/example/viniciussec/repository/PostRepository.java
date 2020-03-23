880272e859bad2580f906a2c61d05961488acb25
viniciussec <viniciussec@hotmail.com>
2020-03-23 19:04:37 -0300

    Criando entity post com User aninhado

--- src/main/java/com/example/viniciussec/repository/PostRepository.java
+++ src/main/java/com/example/viniciussec/repository/PostRepository.java
@@ -3,9 +3,9 @@ package com.example.viniciussec.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.viniciussec.domain.User;
+import com.example.viniciussec.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
-	
+public interface PostRepository extends MongoRepository<Post, String> {
+
 }
