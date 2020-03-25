0eb2c123cdabe758f7dd9339dd49d5a7bae2a221
viniciussec <viniciussec@hotmail.com>
2020-03-24 21:19:55 -0300

    Consulta simples com query methods

--- src/main/java/com/example/viniciussec/resources/PostResource.java
+++ src/main/java/com/example/viniciussec/resources/PostResource.java
@@ -1,26 +1,37 @@
 package com.example.viniciussec.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.viniciussec.domain.Post;
+import com.example.viniciussec.resources.util.URL;
 import com.example.viniciussec.services.PostService;
 
 @RestController
-@RequestMapping(value="/posts")
+@RequestMapping(value = "/posts")
 public class PostResource {
-	
+
 	@Autowired
 	private PostService service;
-	
-	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
-	public ResponseEntity<Post> findById(@PathVariable String id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 
 }
